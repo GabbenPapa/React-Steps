779f141f9c01ec1a40933aaf7609222ed79195be
Bozzay Gábor <111114673+GabbenPapa@users.noreply.github.com>
2025-03-31 22:45:45 +0200

    Add steps layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,61 @@
+import { useState } from "react";
 import "./App.css";
+import "./index.css";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 
 function App() {
-  return <>text</>;
+  const [step, setStep] = useState(1);
+
+  function handlePrev() {
+    setStep((prevStep) => {
+      if (prevStep > 1) {
+        return prevStep - 1;
+      }
+      return prevStep;
+    });
+  }
+
+  function handleNext() {
+    setStep((prevStep) => {
+      if (prevStep < messages.length) {
+        return prevStep + 1;
+      }
+      return prevStep;
+    });
+  }
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
+      </div>
+
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#7950f2", color: "white" }}
+          onClick={handlePrev}
+        >
+          Prev
+        </button>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "white" }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
